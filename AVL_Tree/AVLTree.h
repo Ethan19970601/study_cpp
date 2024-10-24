a2a54ed7b601c04454cd3b212decdd2e68acd77d
ethan <ethan19970601@163.com>
2024-10-24 21:39:42 +0800

    note update: 更新到了旋

--- AVL_Tree/AVLTree.h
+++ AVL_Tree/AVLTree.h
@@ -57,6 +57,32 @@ public:
                 cur->_parent = parent;
             }
 
+            // 更新平衡因子的值
+            while ()
+            {
+                if (cur == parent->_left) // 插入的节点在左边
+                {
+                    parent->_bf--;
+                }
+                else if (cur == parent->_right) // 插入的结点在右边
+                {
+                    parent->_bf++;
+                }
+                if (parent->_bf == 0) // 如果父节点的平衡因子== 0，说明插入的结点刚好让左右子树的高度相等了，这种情况下，我们不需要继续向上更新了
+                {
+                    break; // 不需要更新，直接退出就行了
+                }
+                else if (parent->_bf == 1 || parent->_bf == -1) // 说明插入节点所在的子树高度变化了，要继续往上更新
+                {
+                    cur = parent;
+                    parent = parent->_parent;
+                }
+                else // 走到这里说明平衡因子 _bf!= 0 || _bf != 1 ||  _bf != -1, 出问题了,需要旋转治疗一下了。
+                {
+                    // 旋转
+                }
+            }
+
             return ture;
         }
     }
